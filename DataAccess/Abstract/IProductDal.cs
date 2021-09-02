708ec0cddc2226502911f5d5412ff630c5293004
CASPER <CASPER@DESKTOP-4NI1NQ8>
2021-09-02 11:28:44 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Dal : Data Access Layer  , Dao aynı anlamda kullanılıyor
+    //Product tablosunun veri erişim katmanı
+    public interface IProductDal
+    {
+        //interface metotlari default public
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+        
+        List<Product> GetAllByCategory(int categoryId);//ürünleri kategoriye göre filtrele
+
+    }
+}
