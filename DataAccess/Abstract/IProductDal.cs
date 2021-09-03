098306009fc2986a942eb878a7f693128047230b
beraatkoyuncu <beraat.koyuncu@gmail.com>
2021-09-03 13:54:58 +0300

    global bir katman olan core katmanini olusturdum

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,4 +1,6 @@
-﻿using Entities.Concrete;
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,8 +10,8 @@ namespace DataAccess.Abstract
     //Dal : Data Access Layer  , Dao aynı anlamda kullanılıyor
     //Product tablosunun veri erişim katmanı
     //interface metotlari default public
-    public interface IProductDal:IEntityRepository<Product> //IEntityRepository'i Product için yapılandırmış olduk
+    public interface IProductDal : IEntityRepository<Product> //IEntityRepository'i Product için yapılandırmış olduk
     {
-        
+        List<ProductDetailDto> GetProductDetails();
     }
 }
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,5 +1,7 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,56 +11,18 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
-        public void Add(Product entity)
+        public List<ProductDetailDto> GetProductDetails()
         {
-            //IDispossable pattern implementation of c#
-            //NorthwindContext işi bittiğinde bellekte yer kaplamasın diye using içinde kullandım.
-            using (NorthwindContext context = new NorthwindContext())
+            using (NorthwindContext context=new NorthwindContext())
             {
-                var addedEntity = context.Entry(entity);//referansı yakala
-                addedEntity.State = EntityState.Added;//eklenecek nesne olduğunu belirttik
-                context.SaveChanges();
-
-            }
-        }
-
-        public void Delete(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var deletededEntity = context.Entry(entity);
-                deletededEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            { 
-                //filter=null ise iki noktanın sol tarafı 
-                //filter var ise iki noktanın sağ tarafı. 
-                return filter == null ? context.Set<Product>().ToList() : context.Set<Product>().Where(filter).ToList();
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto { ProductId = p.ProductId, ProductName = p.ProductName, 
+                                 CategoryName = c.CategoryName, UnitsInStock = p.UnitsInStock };
+                return result.ToList();
             }
         }
     }
